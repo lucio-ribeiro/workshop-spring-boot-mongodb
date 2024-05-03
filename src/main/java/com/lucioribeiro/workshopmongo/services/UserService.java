2c650dba631f326ddcad88ba99912b54342a2183
lucio ribeiro <luciorbeio@gmail.com>
2024-05-03 18:06:00 -0300

    Conectando ao mongodb com repository e service

---
+++ src/main/java/com/lucioribeiro/workshopmongo/services/UserService.java
@@ -0,0 +1,20 @@
+package com.lucioribeiro.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lucioribeiro.workshopmongo.domain.User;
+import com.lucioribeiro.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll(){
+		return repo.findAll();
+	}
+}
