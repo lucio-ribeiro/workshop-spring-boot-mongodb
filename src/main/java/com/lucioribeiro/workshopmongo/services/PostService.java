9599d7c7181c47bb0c1c3210a84a7040eb5a31d7
lucio ribeiro <lucioribeiro2009@hotmail.com>
2024-05-08 14:12:33 -0300

    obtendo um post por um id

---
+++ src/main/java/com/lucioribeiro/workshopmongo/services/PostService.java
@@ -0,0 +1,28 @@
+package com.lucioribeiro.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lucioribeiro.workshopmongo.domain.Post;
+import com.lucioribeiro.workshopmongo.repository.PostRepository;
+import com.lucioribeiro.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+		
+    public Post findById(String id) {
+        Optional<Post> postOptional = repo.findById(id);
+        
+        if (postOptional.isEmpty()) {
+            throw new ObjectNotFoundException("Objeto não encontrado");
+        }
+
+        return postOptional.get();
+    }
+
+}
