ac7f58feee5a28273e8df86534af19c72d972402
lucio ribeiro <lucioribeiro2009@hotmail.com>
2024-05-09 10:42:34 -0300

    Consulta simples com query methods

--- src/main/java/com/lucioribeiro/workshopmongo/services/PostService.java
+++ src/main/java/com/lucioribeiro/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.lucioribeiro.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +24,10 @@ public class PostService {
         }
 
         return postOptional.get();
+    }
+    
+    public List<Post> findByTitle(String text) {
+    	return repo.findByTitleContainingIgnoreCase(text);
     }
 
 }
