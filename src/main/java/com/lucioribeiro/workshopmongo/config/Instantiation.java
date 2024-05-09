f1d32b15341041c46952c66bb67ba17e253ba485
lucio ribeiro <lucioribeiro2009@hotmail.com>
2024-05-09 15:02:45 -0300

    Consulta com vários criterios

--- src/main/java/com/lucioribeiro/workshopmongo/config/Instantiation.java
+++ src/main/java/com/lucioribeiro/workshopmongo/config/Instantiation.java
@@ -42,6 +42,7 @@ public class Instantiation implements CommandLineRunner {
 		
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo, Abraços", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		Post post3 = new Post(null, sdf.parse("10/05/2024"), "Bom bom", "Acordei!", new AuthorDTO(bob));
 		
 		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
 		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(alex));
@@ -50,10 +51,13 @@ public class Instantiation implements CommandLineRunner {
 		post1.getComments().addAll(Arrays.asList(c1, c2));
 		post2.getComments().addAll(Arrays.asList(c3));
 		
-		postRepository.saveAll(Arrays.asList(post1, post2));
+		postRepository.saveAll(Arrays.asList(post1, post2, post3));
 		
 		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		alex.getPosts().add(post3);
+		
 		userRepository.save(maria);
+		userRepository.save(alex);
 	}
 
 }
